647b748f913ffe5cf7cea19fe964745af4660d94
yahya mdarhri <78668874+yahya-mdarhri@users.noreply.github.com>
2026-03-25 15:16:48 +0100

    discard

--- management/urls.py
+++ management/urls.py
@@ -7,7 +7,6 @@ from .views import ListActionLogsView, GetActionLogView
 from .views import ListProjectStepsView, CreateProjectStepView, GetProjectStepView, ExecuteProjectStepView
 from .views import ListAllProjectView, ListAllExpensesView, ListAllPaymentsReceivedView
 from .views import DirectorDashboardView, DepartmentDashboardView
-from .views import ListClientsView
 
 
 urlpatterns = [
@@ -15,11 +14,6 @@ urlpatterns = [
 		"departments/",
 		ListDepartmentView.as_view({'get': 'list'}),
 		name="departments"
-	),
-	path(
-		"clients/",
-		ListClientsView.as_view({'get': 'list'}),
-		name="clients"
 	),
 	path(
 		"departments/create/",
